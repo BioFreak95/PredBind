622c0fabbf2ff7676fa6bbfdb825ab069f19a4c7
Maximilian Salomon <maximilian.salomon@fu-berlin.de>
2019-11-11 12:54:11 +0100

    Add variable Augmentation

--- src/CNN/networks/layers/OwnDataset.py
+++ src/CNN/networks/layers/OwnDataset.py
@@ -5,24 +5,32 @@ import h5py
 
 
 class OwnDataset(Dataset):
-    def __init__(self, indices, path):
+    def __init__(self, indices, path, rotations=True):
         self.hdf5file = path
         self.rot = Rotations()
-        #self.indices = indices
-        self.indices = np.tile(indices, 24)
+        self.rotations = rotations
+        if self.rotations:
+            self.indices = np.tile(indices, 24)
+        else:
+            self.indices = indices
         self.length = len(indices)
 
     def __getitem__(self, index):
         data_idx = self.indices[index]
         with h5py.File(self.hdf5file, 'r') as file:
-            #print(file[str(data_idx) + '/data'][()][0])
-            data = self.rot.rotation(
-                data=file[str(data_idx) + '/data'][()][0],
-                k=np.floor_divide(index, self.length)).copy()
+            if self.rotations:
+                data = self.rot.rotation(
+                    data=file[str(data_idx) + '/data'][()][0],
+                    k=np.floor_divide(index, self.length)).copy()
+            else:
+                data = file[str(data_idx) + '/data'][()][0]
             label = file[str(data_idx) + '/label'][()]
             label = -np.log10(np.exp(-label))
 
         return data, label
 
     def __len__(self):
-        return self.length * 24
+        if self.rotations:
+            return self.length * 24
+        else:
+            return self.length
